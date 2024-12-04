bec5016835cea2337e70bfe07b5bd27a03bf4aef
tetopotato <claudiaamador941@gmail.com>
2024-12-03 23:43:48 -0600

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,77 @@
 /*
-
+I used the tool React,
+which I learned how to use from a Udemy course by Angela Yu called "The Comeplete 2024 Web Development Bootcamp", specifically her module on React.
+I learned that React is a JavaScript library used for building user interfaces, normally single page applications but can still render more than that.
+It creates components that manage their own state and automatically updates the user interface when data changes.
+Topics covered:
+Constants & variables- `list` and `taskName` to store data.
+Conditional logic- in the `handleDelete` function to filter tasks based on their ID.
+Iteration- loop through tasks with the `map` method.
+Functions- like `addTask` to manage actions by user.
+Parameters- `id` in the `handleDelete` function.
+Arrays- storing the tasks returned from the `addTask` function.
+Individual elements- accessed in the array to display task title.
+Methods- using `filter` to update the list when task is deleted.
 */
 
+/* 
+Importing useState from React to manage the state in my components (keeps track of tasks and input- data that changes)
+*/
 import { useState } from "react";
 import "./app.css";
 
+/*
+main component of my app that holds all user interface
+holds the array of tasks 
+updates the list when adding/ deleting tasks 
+holds the text the user types 
+updates the name as the user types
+*/
 function Home() {
   const [list, setList] = useState([]);
   const [taskName, setTaskName] = useState("");
 
+
+/* 
+creates a new task and adds it to the list 
+each task is an object with a unique id 
+*/
   function addTask() {
     setList([...list, { id: Date.now(), title: taskName }]);
   }
 
+
+/*
+adds a new task when the ADD button is clicked 
+clears the input box when typing 
+*/
   function handleButton() {
     addTask();
     setTaskName("");
   }
 
+/*
+removes task from the list when DELETE button is clicked 
+new array that keeps tasks that dont match the id of deleted one
+update 
+*/
   function handleDelete(id) {
     const updatedList = list.filter((task) => task.id !== id);
     setList(updatedList);
   }
 
+
+/*
+input
+lets you type task name
+updates taskName while typing
+clicking ADD button calls `handleButton`
+
+todo list 
+loops through tasks in list 
+displays title and delete button
+when clicking DELETE it calls `handleDelete`
+*/
   return (
     <>
       <div className="input-box">
@@ -47,4 +96,8 @@ function Home() {
   );
 }
 
+
+/*
+needed in order to use main component in other files
+*/
 export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,18 @@
+/*
+if you want to use components from other files you must import at top
+using Vite with React (provides a development environment for building React apps) - created basic template
+imported StrictMode from react to write safe code, in case issues with components (included in basic template)
+import createRoot - links react to the root element in index.html
+imported home in order to render my App
+*/
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './App.jsx'
 
+/*
+Reacts created root element that will render and take place of the root element in my index.html
+This is where my Home component is being rendered
+*/
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Home />
